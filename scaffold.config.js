8b5c5a76ab47f6e30e5b7cf6abb35d2219935e01
wessberg <frederikwessberg@gmail.com>
2019-02-09 12:04:47 +0100

    docs(readme): updates logo height

--- scaffold.config.js
+++ scaffold.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	...require("@wessberg/ts-config/scaffold.config"),
 	logo: {
 		url: "https://raw.githubusercontent.com/wessberg/pointer-events/master/documentation/asset/logo.png",
-		height: 150
+		height: 250
 	}
 };
